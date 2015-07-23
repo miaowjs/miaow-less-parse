3e52e63d5d565c337ff8111a67621e019e4c2406
anhulife <anhulife@gmail.com>
2015-07-23 15:29:09 +0800

    使用新的插件封装方式

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var async = require('async');
+var mutil = require('miaow-util');
 
 var importParse = require('./lib/importParse');
 var lessParse = require('./lib/lessParse');
 
-module.exports = function (option, cb) {
+var pkg = require('./package.json');
+
+module.exports = mutil.plugin(pkg.name, pkg.version, function (option, cb) {
   async.series([
     importParse.bind(this, option),
     lessParse.bind(this, option)
   ], cb);
-};
+});
