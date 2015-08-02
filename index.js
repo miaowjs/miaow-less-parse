2427aa1b675f95654c8bbb58b4b67b66f28a963c
anhulife <anhulife@gmail.com>
2015-08-02 21:27:17 +0800

    完善导入插件, 增加对URL处理

--- index.js
+++ index.js
@@ -2,25 +2,38 @@ var less = require('less');
 var mutil = require('miaow-util');
 
 var ImportResolverPlugin = require('./lib/importResolverPlugin');
-var UrlResolverPlugin = require('./lib/urlResolverPlugin');
+var transformUrl = require('./lib/transformUrl');
 var pkg = require('./package.json');
 
 module.exports = mutil.plugin(pkg.name, pkg.version, function (option, cb) {
   var module = this;
-  less.render(
+  less.parse(
     this.contents.toString(),
     {
       filename: this.srcAbsPath,
       relativeUrls: true,
-      plugins: [new ImportResolverPlugin(module), new UrlResolverPlugin(module)]
+      plugins: [new ImportResolverPlugin(module)]
     },
-    function (err, output) {
+    function (err, root, imports, options) {
       if (err) {
         return cb(err);
       }
 
-      module.contents = new Buffer(output.css);
-      cb();
+      transformUrl(module, root, function (err) {
+        if (err) {
+          return cb(err);
+        }
+
+        try {
+          var parseTree = new less.ParseTree(root, imports);
+          var result = parseTree.toCSS(options);
+          module.contents = new Buffer(result.css);
+          cb();
+        } catch (err) {
+          cb(less.formatError(err));
+        }
+      });
     }
   );
-});
+})
+;
--- test/test.js
+++ test/test.js
@@ -27,7 +27,7 @@ describe('miaow-less-parse', function () {
     }, function (err) {
       if (err) {
         console.error(err.toString());
-        throw err;
+        return cb(err);
       }
 
       log = JSON.parse(fs.readFileSync(path.join(output, 'miaow.log.json')));
@@ -42,11 +42,11 @@ describe('miaow-less-parse', function () {
   });
 
   it('编译', function () {
-    assert.equal(log.modules['foo.less'].hash, 'a925e829d0943b56c6025a703f9dba15');
+    assert.equal(log.modules['foo/foo.less'].hash, '5d8e886faabf34df4a8d350cb7524208');
   });
 
   it('添加依赖信息', function () {
-    assert.equal(log.modules['foo.less'].dependList[0], 'bar.less');
-    assert.equal(log.modules['foo.less'].dependList[1], 'bower_components/foz/main.less');
+    assert.equal(log.modules['foo/foo.less'].dependList[0], 'bar.less');
+    assert.equal(log.modules['foo/foo.less'].dependList[1], 'bower_components/foz/main.less');
   });
 });
